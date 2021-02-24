a77436e14beb0e30ee3ed72523641a2e4e3111dc
André <hammel.engineer@gmail.com>
2021-02-24 20:59:22 -0300

    ->Conclusao do item 1: calculos aritmeticos

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,16 @@
+let var1 = 10;
+let var2 = 20;
+
+soma = var1 + var2;
+subtracao = var1 - var2;
+multiplicacao = var1 * var2;
+divisao = var1 / var2;
+modulo = var1 % var2;
+
+console.log("As variaveis são :", var1, " e ", var2);
+
+console.log("Soma das variaveis é:", soma);
+console.log("Subtração das variaveis é:", subtracao);
+console.log("Multiplicação das variaveis é:", multiplicacao);    
+console.log("Divisão das variaveis é:",divisao);
+console.log("Modulo das variaveis é:", modulo);
