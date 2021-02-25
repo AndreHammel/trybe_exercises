bd5a5a7e216221ffc4bd22a74277f1f1945f4f15
André <hammel.engineer@gmail.com>
2021-02-24 23:03:08 -0300

    ->Inclusao do Item_9: dados tres numeros retorna true se algum dele e impar

---
+++ bloco_4/dia_1/item_9.js
@@ -0,0 +1,9 @@
+let numero_1 = 8;
+let numero_2 = 5;
+let numero_3 = 2;
+
+if (numero_1 % 2 != 0 || numero_2 % 2 != 0 || numero_3 % 2 != 0) {
+    console.log(true);
+}else{
+    console.log(false);
+}
