2e63ffb6d1beca5d17735a9287e297e560996019
André <hammel.engineer@gmail.com>
2021-02-24 22:01:00 -0300

    ->Inclusao do item_6: programa da identificaçao do tipo de movimento de peça de xadrez

---
+++ bloco_4/dia_1/item_6.js
@@ -0,0 +1,32 @@
+let namePiece = "Bishop";
+// let transfToLower = namePiece.toLowerCase();
+switch (namePiece.toLowerCase()) {
+    case "pawn":
+        console.log("It moves forward one or two squares.");
+        break;
+
+    case "bishop":
+        console.log("It moves diagonally as many squares as it likes.")
+        break;
+
+    case "knight":
+        console.log("It moves square left or right horizontally.")
+        break;
+
+    case "rook":
+        console.log("It moves left or right, up and down.")
+        break;
+
+    case "queen":
+        console.log("It moves left or right, up and down and diagonally.")
+        break;
+
+    case "king":
+        console.log("It moves one square in any direction.")
+        break;
+    default:
+        console.log("It isn't a chess piece.")
+}
+
+
+// console.log(namePiece.toLowerCase());
