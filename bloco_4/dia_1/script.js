31c91b6a767b5fdae036ae4c4136cfa4fbefac02
André <hammel.engineer@gmail.com>
2021-02-24 21:20:28 -0300

    ->Conclusao do item 4: retorna se numero e positivo, negativo ou zero

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -35,3 +35,16 @@ if (numero1 > numero2 && numero1 > numero3) {
 }else {
     console.log("O numero 3 é o maior número")
 }
+
+//item 4
+
+let variavel_1 = -10;
+
+
+if (variavel_1 > 0) {
+    console.log("O numero é POSITIVO");
+}else if (variavel_1 < 0) {
+    console.log("O numero NEGATIVO");
+}else {
+    console.log("O numero 3 é ZERO")
+}
