8660f248516d8b8a459a36f595723e14c9c88123
André <hammel.engineer@gmail.com>
2021-02-24 21:03:02 -0300

    ->Conclusao do item 2: comparaçao entre variaveis

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,16 +1,26 @@
 let var1 = 10;
 let var2 = 20;
+console.log("As variaveis são :", var1, " e ", var2);
 
+//item 1
 soma = var1 + var2;
 subtracao = var1 - var2;
 multiplicacao = var1 * var2;
 divisao = var1 / var2;
 modulo = var1 % var2;
 
-console.log("As variaveis são :", var1, " e ", var2);
-
 console.log("Soma das variaveis é:", soma);
 console.log("Subtração das variaveis é:", subtracao);
 console.log("Multiplicação das variaveis é:", multiplicacao);    
 console.log("Divisão das variaveis é:",divisao);
 console.log("Modulo das variaveis é:", modulo);
+console.log("\n");
+
+//item 2
+if (var1 > var2) {
+    console.log("A variavel var1 é maior que a var2");
+}else {
+    console.log("A variavel var2 é maior que a var1");
+}
+
+//item 3
