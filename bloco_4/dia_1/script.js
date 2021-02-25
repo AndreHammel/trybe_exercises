d8600d4050b812b0912498edbf94c1d083b3302b
André <hammel.engineer@gmail.com>
2021-02-24 21:17:13 -0300

    ->Conclusao do item 3: maior numero entre 3 numeros

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -24,3 +24,14 @@ if (var1 > var2) {
 }
 
 //item 3
+let numero1 = 100;
+let numero2 = 200;
+let numero3 = 300;
+
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log("O numero 1 é o maior número");
+}else if (numero2 > numero1 && numero2 > numero3) {
+    console.log("O numero 2 é o maior número");
+}else {
+    console.log("O numero 3 é o maior número")
+}
