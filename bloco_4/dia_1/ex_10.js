156fa3cac05a459a188722b8308d923fc937c321
André <hammel.engineer@gmail.com>
2021-02-27 12:43:11 -0300

    ->Mudança dos nomes dos arquivos, inclusao exercicio 10

---
+++ bloco_4/dia_1/ex_10.js
@@ -0,0 +1,10 @@
+let valorCusto = 100;
+let valorVenda = 140;
+let impostoSobreCusto = 20;
+
+
+let valorCustoTotal = valorCusto + (valorCusto * (impostoSobreCusto / 100));
+let lucro = (valorVenda - valorCustoTotal) * 1000;
+
+console.log(`Custo por unidade de produto: ${valorCustoTotal}`);
+console.log(`Lucro sob a venda de 1000 unidades : ${lucro}`);
